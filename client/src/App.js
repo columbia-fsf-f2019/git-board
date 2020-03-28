9c8ef074016e82e48669196698e347056e297c1a
russomp <russo.michaelp@gmail.com>
2020-03-28 11:09:22 -0400

    wip: Render todolist at app level

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    };
+  }
+
+  async componentDidMount() {
+    const res = await fetch('/api/todos');
+    const todos = await res.json();
+    console.log(`[DEBUG] App(cDM) todos :: ${JSON.stringify(todos, null, 2)}`);
+    this.setState({ todos: todos });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Todoloo!</h1>
+        <ul>
+          {this.state.todos.map((todo) => (
+            <li key={todo.id}>{todo.text}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders welcome to todoloo header', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElem = getByText(/welcome to todoloo/i);
+  expect(headerElem).toBeInTheDocument();
 });
