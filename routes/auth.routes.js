796bbcd52793ce6399c2f9788767e99909b4372d
russomp <russo.michaelp@gmail.com>
2020-04-02 23:43:29 -0400

    refactor: Extract login logic into auth controller

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,23 +1,9 @@
 const router = require('express').Router();
 
-const db = require('../models');
+const authControllers = require('../controllers/auth.controllers')
 const auth = require('../utils/auth');
 
-router.get('/me', auth.required, (req, res) => {
-  res.send({ user: req.auth.user.id });
-});
-
-router.post('/login', async (req, res) => {
-  const { email: enteredEmail, password: enteredPassword } = req.body;
-  const user = await db.User.scope(null).findOne({ where: { email: enteredEmail } });
-
-  if (!user) return res.status(404).json({ message: 'User not found' });
-
-  const isPasswordCorrect = await user.checkPassword(enteredPassword);
-  if (!isPasswordCorrect)
-    return res.status(400).json({ message: 'Incorrect password' });
-
-  return res.json({ token: user.generateToken() });
-});
+router.get('/me', auth.required, authControllers.me);
+router.post('/login', authControllers.login);
 
 module.exports = router;
